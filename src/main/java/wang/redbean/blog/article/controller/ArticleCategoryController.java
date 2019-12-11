764d48566cb27570811b32af822786080f607775
Dell <shipeng_w@163.com>
2019-12-11 17:35:39 +0800

    个人博客开发2019-12-11

--- src/main/java/wang/redbean/blog/article/controller/ArticleCategoryController.java
+++ src/main/java/wang/redbean/blog/article/controller/ArticleCategoryController.java
@@ -1,17 +1,17 @@
 package wang.redbean.blog.article.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wang.redbean.blog.article.entity.ArticleCategory;
 import wang.redbean.blog.article.entity.vo.ResultVo;
 import wang.redbean.blog.article.serivce.IArticleCategoryService;
 import wang.redbean.blog.article.serivce.impl.ArticleCategoryServiceImpl;
 import wang.redbean.blog.common.base.controller.BaseController;
+import wang.redbean.blog.common.base.entity.response.ServerResponse;
+import wang.redbean.blog.common.util.Tools;
 import wang.redbean.blog.utils.ResultVoUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -26,34 +26,53 @@ public class ArticleCategoryController extends BaseController<IArticleCategorySe
     private ArticleCategoryServiceImpl articleCategoryService;
 
     @PostMapping("/save")
-    public ResultVo save(ArticleCategory articleCategory){
-        boolean result = articleCategoryService.save(articleCategory);
-        if(result){
-            return ResultVoUtil.success();
+    public ServerResponse save(ArticleCategory articleCategory){
+        if(Tools.IsTypeExist(articleCategory.getCategoryType(),articleCategoryService)){
+            return ServerResponse.createByErrorMessage("保存失败,该类目类型已存在");
         }else{
-            return ResultVoUtil.error(400,"保存失败");
+            boolean result = articleCategoryService.save(articleCategory);
+            if(result){
+                return ServerResponse.createBySuccessMessage("保存成功");
+            }else{
+                return ServerResponse.createByErrorMessage("保存失败");
+            }
         }
+
     }
 
     @GetMapping("/delete")
-    public ResultVo delete(List<Integer> idList){
+    public ServerResponse delete(@RequestParam(value = "idList",required = true) List<Integer> idList){
         Integer result = articleCategoryService.deleteByIds(idList);
         if(result>0){
-            return ResultVoUtil.success(result);//result，返回删除行数
+            return ServerResponse.createBySuccessMessage(result+"");//result，返回删除行数
         }else{
-            return ResultVoUtil.error(400,"删除失败");
+            return ServerResponse.createByErrorMessage("删除失败");
         }
     }
 
     @PostMapping("/update")
-    public ResultVo update(ArticleCategory articleCategory){
-        boolean result = articleCategoryService.updateById(articleCategory);
-        if(result){
-            return ResultVoUtil.success();
+    public ServerResponse update(ArticleCategory articleCategory){
+        if(Tools.IsTypeExist(articleCategory.getCategoryType(),articleCategoryService)){
+            return ServerResponse.createByErrorMessage("修改失败,该类目类型已存在");
         }else{
-            return ResultVoUtil.error(400,"修改失败");
+            boolean result = articleCategoryService.updateById(articleCategory);
+            if(result){
+                return ServerResponse.createByErrorMessage("修改成功");
+            }else{
+                return ServerResponse.createByErrorMessage("修改失败");
+            }
         }
+
     }
 
+    @GetMapping("/get")
+    public ServerResponse get(HttpServletRequest request,ArticleCategory articleCategory){
+       List<ArticleCategory> articleCategoryList = articleCategoryService.getByParam(request,articleCategory);
+       if(articleCategoryList.size()>0){
+           return ServerResponse.createBySuccess("查询成功",articleCategoryList);
+       }else{
+           return  ServerResponse.createByErrorMessage("查询失败");
+       }
+    }
 
 }
