6305469feca0b7b174913a7a15c68ef42adf4aa4
Dell <shipeng_w@163.com>
2019-12-14 10:19:24 +0800

    个人博客开发2019-12-14

--- src/main/java/wang/redbean/blog/article/controller/ArticleInfoController.java
+++ src/main/java/wang/redbean/blog/article/controller/ArticleInfoController.java
@@ -1,14 +1,79 @@
 package wang.redbean.blog.article.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import wang.redbean.blog.article.entity.ArticleInfo;
+import wang.redbean.blog.article.entity.ArticleRecived;
 import wang.redbean.blog.article.serivce.IArticleInfoService;
+import wang.redbean.blog.article.serivce.impl.ArticleInfoServiceImpl;
 import wang.redbean.blog.common.base.controller.BaseController;
+import wang.redbean.blog.common.base.entity.response.ServerResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * 章详情控制器类
  */
 @RestController
-@RequestMapping("/article_info")
+@RequestMapping("/article/info")
 public class ArticleInfoController extends BaseController<IArticleInfoService> {
+
+    @Autowired
+    private ArticleInfoServiceImpl articleInfoService;
+
+    /**
+     * 新增一个用户
+     * @param session
+     * @param articleRecived
+     * @return
+     */
+    @PostMapping("save")
+    public ServerResponse save(HttpSession session, ArticleRecived articleRecived){
+//        articleRecived.setUserId(getUidFromSession(session));
+        articleRecived.setUserId(3);
+        Integer result = articleInfoService.insert(articleRecived);
+        if(result == 1){
+            return ServerResponse.createBySuccessMessage("保存成功");
+        }else{
+            return  ServerResponse.createByErrorMessage("保存失败");
+        }
+    }
+
+    /**
+     * 删除一篇文章
+     * @param idList
+     * @return
+     */
+    @GetMapping("/delete")
+    public ServerResponse delete(@RequestParam(value = "idList",required=true) List<Integer> idList){
+        Integer result = articleInfoService.deleteByIds(idList);
+        if(result>0){
+            return  ServerResponse.createBySuccess("删除成功",result);
+        }else{
+            return  ServerResponse.createByErrorMessage("删除失败");
+        }
+    }
+
+    @PostMapping("/update")
+    public  ServerResponse update(ArticleInfo articleInfo){
+        Integer result = articleInfoService.updateInfoById(articleInfo);
+        if(result>0){
+            return  ServerResponse.createBySuccessMessage("修改成功");
+        }else{
+            return  ServerResponse.createByErrorMessage("修改失败");
+        }
+    }
+
+    @GetMapping("/get")
+    public ServerResponse get(HttpServletRequest request ,ArticleInfo articleInfo){
+        List<ArticleInfo> articleInfoList =articleInfoService.getByParam(request,articleInfo);
+        if(articleInfoList.size()>0){
+            return ServerResponse.createBySuccess("查询成功",articleInfoList);
+        }else{
+            return  ServerResponse.createByErrorMessage("未查询到文章信息");
+        }
+    }
 }
