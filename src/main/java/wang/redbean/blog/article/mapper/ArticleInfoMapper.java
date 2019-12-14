690164c18c486ae8e20cde14a38b4fb220eabf6d
Dell <shipeng_w@163.com>
2019-12-14 10:20:03 +0800

    个人博客开发2019-12-11

--- src/main/java/wang/redbean/blog/article/mapper/ArticleInfoMapper.java
+++ src/main/java/wang/redbean/blog/article/mapper/ArticleInfoMapper.java
@@ -1,12 +1,33 @@
 package wang.redbean.blog.article.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.*;
+import wang.redbean.blog.article.entity.ArticleContent;
 import wang.redbean.blog.article.entity.ArticleInfo;
+import wang.redbean.blog.article.entity.ArticleRecived;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * 文章详情映射接口类
  */
 public interface ArticleInfoMapper extends BaseMapper<ArticleInfo> {
+
+    @Insert("save")
+    Integer save(@Param("articleRecived")ArticleRecived articleRecived);
+
+    @Delete("deleteByIds")
+    Integer deleteByIds(@Param("idList")List<Integer> idList);
+
+    @Update("updateInfoById")
+    Integer updateInfoById(@Param("articleInfo") ArticleInfo articleInfo);
+
+    @Select("findAll")
+    List<ArticleInfo> findAll();
+
+    @Select("getByParam")
+    List<ArticleInfo> getByParam(@Param("articleInfo") ArticleInfo articleInfo, @Param("startTime") Date startTime, @Param("endTime") Date endTime);
 
 
 }
